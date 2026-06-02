f137750d4b20ea014b9546d660092b3757348de6
gisuk <gisuk0222@gmail.com>
2026-06-02 20:26:37 +0900

    fix: fastAPI 에러 해결

--- backend/fastapi/app/models/schemas.py
+++ backend/fastapi/app/models/schemas.py
@@ -170,12 +170,8 @@ class DiaryRequest(BaseModel):
     matched: Optional[bool] = None
     tags: Optional[List[str]] = []
     group_description: Optional[str] = None
-<<<<<<< HEAD
-    photo_count: Optional[int] = None
-    room_category: Optional[str] = None     # 모임방 테마 카테고리 (EXERCISE, HOBBY 등)
-=======
     photo_count: Optional[int] = 1
->>>>>>> c0552f4e919b8e7cedb60f30cb817a95d57c117f
+    room_category: Optional[str] = None     # 모임방 테마 카테고리 (EXERCISE, HOBBY 등)
 
 class DiaryResponse(BaseModel):
     title: str
