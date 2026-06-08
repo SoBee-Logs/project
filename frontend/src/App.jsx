13b76358d0a50a1d4fad38d289deaf21a4d2c680
seokbeen <blackwiz09@gmail.com>
2026-06-08 14:39:31 +0900

    style: 자잘한 ui 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const routeConfig = [
   { path: '/home',           element: <Home />,         bottomNav: true,  floatingNav: true,  appBar: false },
   { path: '/home/detail',    element: <HomeDetail />,   bottomNav: false, floatingNav: false, appBar: true, title: '홈 상세' },
   { path: '/report',         element: <AvaterRoom />,   bottomNav: true,  floatingNav: false, appBar: true,  title: '리포트', noScroll: true },
-  { path: '/report/monthly', element: <Report />,       bottomNav: false, floatingNav: false, appBar: true,  title: '리포트' },
+  { path: '/report/monthly', element: <Report />,       bottomNav: true,  floatingNav: false, appBar: true,  title: '리포트' },
   { path: '/report/detail',  element: <ReportDetail />, bottomNav: false, floatingNav: false, appBar: true,  title: '리포트 상세' },
   { path: '/feed',           element: <Feed />,         bottomNav: true,  floatingNav: false, appBar: false },
   { path: '/camera',         element: <CameraPage />,   bottomNav: false, floatingNav: false, appBar: false },
