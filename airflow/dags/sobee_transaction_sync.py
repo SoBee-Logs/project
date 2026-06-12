1d82cb38f5a91695d2288b7cd4b8263d547a40be
thdudgus <gusthdud527@cau.ac.kr>
2026-06-13 02:23:16 +0900

    fix: Airflow 3.0 dag-processor 서비스 추가 및 deprecated 연산자 수정
    - docker-compose.yml에 airflow-dag-processor 서비스 추가
      (Airflow 3.0에서 DAG 파일 파싱은 scheduler가 아닌 dag-processor 담당)
    - sobee_transaction_sync.py의 deprecated import 경로 수정
      airflow.operators.python → airflow.providers.standard.operators.python

--- airflow/dags/sobee_transaction_sync.py
+++ airflow/dags/sobee_transaction_sync.py
@@ -147,7 +147,7 @@ def task_persona(**ctx):
 with DAG(
     dag_id="sobee_transaction_sync",
     description="매일 sync, 월요일 photo 있는 유저 아바타 생성",
-    schedule="0 17 * * *",  # KST 02:00 (UTC 17:00)
+    schedule="30 17 * * *",  # KST 02:30 (UTC 17:30)
     start_date=datetime(2026, 1, 1),
     catchup=False,
     tags=["sobee", "transaction", "persona"],
